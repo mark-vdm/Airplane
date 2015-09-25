211ad96e2beef302bbeecb0513f514491e553d36
Mark PC win10 <majik_mark@hotmail.com>
2015-09-25 17:40:24 -0400

    Adding another file

---
+++ Airplane.cpp
@@ -0,0 +1,14 @@
+#include "Airplane.h"
+
+Airplane::Airplane(){
+    //Default constructor
+}
+/*
+bool Airplane::create_file(){
+    return 1;
+}
+*/
+
+void Airplane::outpt(){
+    Serial.println("I'm here");
+}
